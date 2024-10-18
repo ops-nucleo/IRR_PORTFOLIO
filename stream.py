ac3b99b46575eccd4e6bfa8f82e0a85c5bbd2fd7
ops-nucleo <163151317+ops-nucleo@users.noreply.github.com>
2024-10-18 15:38:37 -0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -127,13 +127,13 @@ if st.session_state['acesso_permitido']:
 
             # Ajustar o título e labels com tamanhos de fonte reduzidos
             ax.set_title(f"{empresa} - {variavel} de {data_de.strftime('%d/%m/%Y')} até {data_ate.strftime('%d/%m/%Y')}",
-                         fontsize=14)  # Tamanho do título menor
-            ax.set_xlabel("Data", fontsize=12)  # Tamanho do rótulo do eixo X
-            ax.set_ylabel(variavel, fontsize=12)  # Tamanho do rótulo do eixo Y
+                         fontsize=10)  # Tamanho do título menor
+            ax.set_xlabel("Data", fontsize=8)  # Tamanho do rótulo do eixo X
+            ax.set_ylabel(variavel, fontsize=8)  # Tamanho do rótulo do eixo Y
         
             # Ajustar os ticks (marcas de escala no gráfico) com fontes menores
-            ax.tick_params(axis='x', labelsize=10)  # Tamanho das datas no eixo X
-            ax.tick_params(axis='y', labelsize=10)  # Tamanho dos valores no eixo Y
+            ax.tick_params(axis='x', labelsize=6)  # Tamanho das datas no eixo X
+            ax.tick_params(axis='y', labelsize=6)  # Tamanho dos valores no eixo Y
 
             # Definir o formato da data no eixo X
             ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))  # Formato dd/mm/aaaa
