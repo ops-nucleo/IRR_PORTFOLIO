265a7dd7239d081633431fe23c93ef416242ee48
ops-nucleo <163151317+ops-nucleo@users.noreply.github.com>
2025-02-07 16:53:32 -0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -224,22 +224,13 @@ if st.session_state['acesso_permitido']:
             for col in colunas_listadas:
                 html += f'<th style="border: 1px solid #ddd; padding: 8px; text-align: center;">{col}</th>'
             html += '</tr></thead><tbody>'
-            for _, row in df.iterrows():
-                html += '<tr>'
-                for col in df.columns:
-                    html += f'<td style="border: 1px solid #ddd; padding: 8px; text-align: center;">{row[col]}</td>'
-                html += '</tr>'
-            # Linhas alternadas (cinza e branco)
             for i, row in df.iterrows():
-                if i % 2 == 0:
-                    bg_color = 'rgb(242, 242, 242)'  # Cinza
-                else:
-                    bg_color = 'white'  # Branco
-        
+                bg_color = 'rgb(242, 242, 242)' if i % 2 == 0 else 'white'
                 html += f'<tr style="background-color: {bg_color}; color: black;">'
                 for col in df.columns:
                     html += f'<td style="border: 1px solid #ddd; padding: 8px; text-align: center; color: black;">{row[col]}</td>'
                 html += '</tr>'
+
             html += '</tbody></table>'
             return html
     
