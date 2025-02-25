046eb8d4a6d36f9e0225c97baacfb5b1b844d89d
ops-nucleo <163151317+ops-nucleo@users.noreply.github.com>
2025-02-25 15:53:18 -0300

    Update stream.py

--- stream.py
+++ stream.py
@@ -55,56 +55,47 @@ if st.session_state['acesso_permitido']:
     set_background(get_image_as_base64(logo_path))
 
 
-    st.markdown("""
-        <style>
-            div[role="radiogroup"] {
-                display: flex;
-                justify-content: left;
-                gap: 10px;
-            }
+     st.markdown("""
+            <style>
+                div[role="radiogroup"] {
+                    display: flex;
+                    justify-content: left;
+                    gap: 10px;
+                }
     
-            /* Estilizando os botões de rádio */
-            div[role="radiogroup"] label {
-                background-color: rgb(0, 32, 96); /* Azul Nucleo Capital */
-                color: white !important; /* Texto branco */
-                padding: 12px 24px; /* Aumenta o tamanho do botão */
-                border-radius: 8px;
-                font-weight: normal;
-                cursor: pointer;
-                transition: 0.3s;
-                text-align: center;
-                border: 2px solid transparent;
-                display: block; /* Faz com que toda a área seja clicável */
-            }
+                /* Estilizando os botões de rádio quando NÃO estão selecionados */
+                div[role="radiogroup"] label {
+                    background-color: rgb(0, 32, 96); /* Azul Nucleo Capital */
+                    color: white !important; /* Texto branco */
+                    padding: 10px 21px;
+                    border-radius: 8px;
+                    font-weight: normal;
+                    cursor: pointer;
+                    transition: 0.3s;
+                    text-align: center;
+                    display: block; /* Faz com que toda a área seja clicável */
+                    border: 2px solid transparent;
+                }
+        
+                /* Quando o botão NÃO está selecionado */
+                div[role="radiogroup"] div {
+                    color: white;
+                }
+                    /* Força a cor branca no texto dentro do botão não selecionado */
+                div[role="radiogroup"] label span {
+                    color: white !important; 
+                }
     
-            /* Quando o botão NÃO está selecionado */
-            div[role="radiogroup"] div {
-                color: white;
-            }
-    
-            /* Texto dentro do botão não selecionado */
-            div[role="radiogroup"] label span {
-                color: white !important; 
-            }
-    
-            /* Quando o botão está selecionado */
-            div[role="radiogroup"] label:has(input:checked) {
-                background-color: white;
-                color: rgb(0, 32, 96);
-                border: 2px solid rgb(0, 32, 96);
-            }
-    
-            /* Ajuste para evitar problemas na detecção do clique */
-            div[role="radiogroup"] input {
-                opacity: 0;
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                cursor: pointer;
-            }
-        </style>
-    """, unsafe_allow_html=True)
-
+                /* Quando o botão está selecionado */
+                div[role="radiogroup"] input:checked + div {
+                    background-color: rgb(0, 32, 96);
+                    color: white;
+                    border: 2px solid rgb(0, 32, 96);
+                    text-align: left;
+                    cursor: pointer;
+                }
+            </style>
+        """, unsafe_allow_html=True)
     class TabelaPortfolioLucro:
         def __init__(self, df_empresa):
             # Converte a coluna 'DATA ATUALIZACAO' para datetime
